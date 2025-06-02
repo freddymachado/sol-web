f43111af57befd6c5584e1fc5ebdba24adcfc38b
freddymachado <freddyantoniomachado@gmail.com>
2025-06-02 16:35:14 -0400

    debugging cors policy

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -51,14 +51,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "c13e77d82ff0e80a6e087ef340fe0286
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "f6c98d06df7d01785afcdd6da7ad0508",
+"flutter_bootstrap.js": "5afa6d4b65bbc725989d7f4a2562fd86",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "08c7d2199a8ff0b5903f45f758aebf38",
-"/": "08c7d2199a8ff0b5903f45f758aebf38",
-"main.dart.js": "6738296a4f207a8078e3562c9c023ab5",
+"index.html": "4825aad50faa2081ab53a14e1c673d95",
+"/": "4825aad50faa2081ab53a14e1c673d95",
+"main.dart.js": "f0ba652b5b6e1a9a15d9eb68426e1b96",
 "manifest.json": "2d30e47a796a2f48a3f936e41290e7de",
 "version.json": "18e550ffd60ca982e5ce27eed778346e"};
 // The application shell files that are downloaded before a service worker can
