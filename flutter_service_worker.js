04224e5eceadbe80ebf8a74b2371f6c2d68f39ee
freddymachado <freddyantoniomachado@gmail.com>
2025-08-05 00:54:29 -0400

    :construction: deploy automate test10

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -78,13 +78,13 @@ const RESOURCES = {"assets/AssetManifest.bin": "b07bb514741ed6ffea4601a978dc95a5
 "favicon.png": "1e3b9b2b8748cd5b7a2bbfc2ab317abd",
 "favicon2.png": "d6e27b457f70aea0508f49212d32ec7a",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "13ef0a3f6a7f6f2c1899fe078bb2c5a4",
+"flutter_bootstrap.js": "94f2b9b6fb26df247d5a517068d1167e",
 "icons/Icon-192.png": "954e34c671aec62fe26c3a2fbb6034dc",
 "icons/Icon-512.png": "1e3b9b2b8748cd5b7a2bbfc2ab317abd",
-"index.html": "7c8fc1d4df46ec4fe6f211abf7830f25",
-"/": "7c8fc1d4df46ec4fe6f211abf7830f25",
+"index.html": "da6b783476250176cb61ed483466a732",
+"/": "da6b783476250176cb61ed483466a732",
 "main.dart.js": "f314811dbd822feb0a58cad15774e986",
-"manifest.json": "ecb0b5306dbd56c38718509c650662c7",
+"manifest.json": "71460a8c9851baa970d2c4e715867a7d",
 "version.json": "18e550ffd60ca982e5ce27eed778346e"};
 // The application shell files that are downloaded before a service worker can
 // start.
