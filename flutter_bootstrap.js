81cdb31fe8309b38fbdb72b54f79452e1c540fb5
freddymachado <freddyantoniomachado@gmail.com>
2025-08-04 23:41:33 -0400

    :construction: deploy automate test1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2654732244"
+    serviceWorkerVersion: "3224030544"
   }
 });
